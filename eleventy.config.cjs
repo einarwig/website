09f4fab56e7875bdc76630cece45b7c39f128216
Gunnar Kriik <gunnar@kriik.com>
2026-01-29 22:30:15 +0100

    fixed config

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
 
     return {
-        templateFormats: ["html", "gif", "jpg", "css"],  
+        templateFormats: ["html", "gif", "jpg", "jpeg","css"],  
 // This sets the default formats
         dir: {
             input: ".",
